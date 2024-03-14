5d9f0e7fa5a3f9ef2a8a1f1004fb7965c286ea09
Nykk Romancer <nykk.romancer@gmail.com>
2024-03-14 12:24:23 -1000

    Initial commit

---
+++ SOD_DarkmoonFaire.lua
@@ -0,0 +1,131 @@
+-- Define the namespace if it doesn't exist
+NykkRomancer = NykkRomancer or {}
+
+NykkRomancer.SOD_FairItems = NykkRomancer.SOD_FairItems or {
+    [5134] = {
+        Name = "Small Furry Paw",
+        QuestGiver = "Yebb Neblegear",
+        Quantity = 5,
+        Tier = 1,
+    },
+    [11407] = {
+        Name = "Torn Bear Pelt",
+        QuestGiver = "Yebb Neblegear",
+        Quantity = 5,
+        Tier = 2,
+    },
+    [4582] = {
+        Name = "Soft Bushy Tail",
+        QuestGiver = "Yebb Neblegear",
+        Quantity = 5,
+        Tier = 3,
+    },
+    [5117] = {
+        Name = "Vibrant Plume",
+        QuestGiver = "Yebb Neblegear",
+        Quantity = 5,
+        Tier = 4,
+    },
+    [11404]= {
+        Name = "Evil Bat Eye",
+        QuestGiver = "Yebb Neblegear",
+        Quantity = 10,
+        Tier = 5,
+    },
+    [19933]= {
+        Name = "Glowing Scorpid Blood",
+        QuestGiver = "Yebb Neblegear",
+        Quantity = 10,
+        Tier = 5,
+    },
+    [3240]= {
+        Name = "Coarse Weightstone",
+        QuestGiver = "Kerri Hicks",
+        Quantity = 10,
+        Tier = 1,
+    },
+    [3486]= {
+        Name = "Heavy Grinding Stone",
+        QuestGiver = "Kerri Hicks",
+        Quantity = 7,
+        Tier = 2,
+    },
+    [3835]= {
+        Name = "Green Iron Bracers",
+        QuestGiver = "Kerri Hicks",
+        Quantity = 3,
+        Tier = 3,
+    },
+    [7945]= {
+        Name = "Big Black Mace",
+        QuestGiver = "Kerri Hicks",
+        Quantity = 1,
+        Tier = 4,
+    },
+    [12644]= {
+        Name = "Dense Grinding Stone",
+        QuestGiver = "Kerri Hicks",
+        Quantity = 8,
+        Tier = 5,
+    },
+    [4363]= {
+        Name = "Copper Modulator",
+        QuestGiver = "Rinling",
+        Quantity = 5,
+        Tier = 1,
+    },
+    [4375]= {
+        Name = "Whirring Bronze Gizmo",
+        QuestGiver = "Rinling",
+        Quantity = 7,
+        Tier = 2,
+    },
+    [9313]= {
+        Name = "Green Firework",
+        QuestGiver = "Rinling",
+        Quantity = 36,
+        Tier = 3,
+    },
+    [11590]= {
+        Name = "Michanical Repair Kit",
+        QuestGiver = "Rinling",
+        Quantity = 6,
+        Tier = 4,
+    },
+    [15994]= {
+        Name = "Thorium Widget",
+        QuestGiver = "Rinling",
+        Quantity = 6,
+        Tier = 5,
+    },
+    [2309]= {
+        Name = "Embossed Leather Boots",
+        QuestGiver = "Chronos",
+        Quantity = 3,
+        Tier = 1,
+    },
+    [2314]= {
+        Name = "Toughened Leather Armor",
+        QuestGiver = "Chronos",
+        Quantity = 3,
+        Tier = 2,
+    },
+    [5739]= {
+        Name = "Barbaric Harness",
+        QuestGiver = "Chronos",
+        Quantity = 3,
+        Tier = 3,
+    },
+    [8185]= {
+        Name = "Turtle Scale Leggings",
+        QuestGiver = "Chronos",
+        Quantity = 1,
+        Tier = 4,
+    },
+    [15564]= {
+        Name = "Rugged Armor Kit",
+        QuestGiver = "Chronos",
+        Quantity = 8,
+        Tier = 5,
+    },
+}
